ee2224b2ad53a812cbb4c4bdd631d6849e9e2c00
Matthew Molinar <matthew.a.molinar@gmail.com>
2024-07-03 02:29:28 -0500

    template commit

---
+++ lib/game/entities/unicorn/behaviors/tapping_behavior.dart
@@ -0,0 +1,25 @@
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flame/components.dart';
+import 'package:flame/events.dart';
+import 'package:flame_behaviors/flame_behaviors.dart';
+import 'package:test_1_game/game/game.dart';
+import 'package:test_1_game/gen/assets.gen.dart';
+
+class TappingBehavior extends Behavior<Unicorn>
+    with TapCallbacks, HasGameRef<Test1Game> {
+  @override
+  bool containsLocalPoint(Vector2 point) {
+    return parent.containsLocalPoint(point);
+  }
+
+  @override
+  void onTapDown(TapDownEvent event) {
+    if (parent.isAnimationPlaying()) {
+      return;
+    }
+    gameRef.counter++;
+    parent.playAnimation();
+
+    gameRef.effectPlayer.play(AssetSource(Assets.audio.effect));
+  }
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:test_1_game/app/app.dart';
+import 'package:test_1_game/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
